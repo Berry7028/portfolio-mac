a1babd5c1d47262b78cc9223354bf974c5bec41d
kouta <s.kouta7028@gmail.com>
2026-03-19 13:47:51 +0900

    fix: page.tsxのh1テキストのfontを変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import Header from "@/app/components/Header";
+import { Inspiration } from "next/font/google";
+
+const inspiration = Inspiration({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Home() {
   return (
-    <div className=" text-black">
+    <div className="text-black">
       <Header />
       <div className="flex items-center justify-center h-screen">
-        <h1>Hello World</h1>
+        <div className={inspiration.className}>
+          <h1 className="text-9xl">Please launch Safari</h1>
+        </div>
       </div>
     </div>
   );
